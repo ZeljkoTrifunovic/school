46f9830d7f9835acf0af03e86c02a0b2ef0a8589
ZeljkoTrifunovic <zecatrifunovic@gmail.com>
2020-02-29 07:48:24 -0800

    Napravljen paket media i klasa television

---
+++ src/main/java/media/Television.java
@@ -0,0 +1,33 @@
+
+package media;
+
+
+public class Television {
+    private int volume = 0;
+    private  int currentProgram = 1;
+    private boolean turnOn = true;
+    
+    public int getVolume(){
+        return this.volume;
+    }
+    
+    public void setVolume(int customVolume){
+        this.volume = customVolume;
+    }
+    
+    public int getCurrentProgram(){
+        return this.currentProgram;
+    }
+    
+    public void setCurrentProgram(int customCurrentProgram){
+        this.currentProgram = customCurrentProgram;
+    }
+    
+    public boolean getTurnOn(){
+        return this.turnOn;
+    }
+    public void setTurnOn(boolean customTurnOn){
+         this.turnOn = customTurnOn;
+    }
+    
+}
